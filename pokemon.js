b14cc53ae828bf9abae538fbc40214ed4d98791a
seedback <seedback2@gmail.com>
2021-11-19 15:23:04 +0100

    add prettyfication

--- pokemon.js
+++ pokemon.js
@@ -1,52 +1,45 @@
-/*
-const temp = [[1, 4], [3, 4], [0, 1], [0, 2], [2, 3]]
-
-p-choice = 2
-
-ai-choice = 0
-
-p-beats = temp[2] = [0, 1]
-
-ai-beats = temp[2] = [1, 4]
-
-if (p-beats[0] == ai-choice || p-beats[1] == ai-choice){
-	player wins!
-}
-else if(ai-beats[0] == p-choice || ai-beats[1] == p-choice){
-	ai wins!
-}
-else{
-	draw!
-}
-*/
-
 // Indexes:				  0				1				2				3				4
 const pokeBeat = [[1, 4], [3, 4], [0, 1], [0, 2], [2, 3]];
 const pokeNames = ["Vulpix", "Glaceon", "Geodude", "Cubone", "Turtwig"];
 const pokeImages = [
-	`<img src="img/vulpix.png" width="200px" height="200px"/>`,
-	`<img src="img/glaceon.png" width="200px" height="200px"/ >`,
-	`<img src="img/geodude.png" width="200px" height="200px"/>`,
-	`<img src="img/cubone.png" width="200px" height="200px"/ >`,
-	`<img src="img/turtwig.png" width="200px" height="200px"/>`
+	`<img src="img/vulpix.png" />`,
+	`<img src="img/glaceon.png"/>`,
+	`<img src="img/geodude.png"/>`,
+	`<img src="img/cubone.png" />`,
+	`<img src="img/turtwig.png"/>`
 ]
 
 var playerChoiceIndex;
 var aiChoiceIndex;
 
 var textBoxContent;
+var textBoxVar;
 
 var playerChoiceDivContent;
 var aiChoiceDivContent;
 
+var canPlayerChoose = true;
 
 function playerChoice(choice) {
-  playerChoiceIndex = choice;
-  console.log("playerChoiceIndex:", playerChoiceIndex);
-  aiChoice();
-  showPlayerChoice();
-  showAiChoice();
-  findWinner();
+	if(canPlayerChoose){
+		canPlayerChoose = false;
+		playerChoiceDivContent = "";
+		aiChoiceDivContent = "";
+
+		playerChoiceIndex = choice;
+		console.log("playerChoiceIndex:", playerChoiceIndex);
+		aiChoice();
+
+		setTimeout( showPlayerChoice, 0);
+		setTimeout(textBox, 0, `You chose ${pokeNames[playerChoiceIndex]}!`, 3000)
+
+		setTimeout(showAiChoice, 5000);
+		setTimeout(textBox, 5000, `Your opponent chose ${pokeNames[aiChoiceIndex]}!`, 3000)
+		
+		setTimeout( findWinner, 10000);
+		
+		setTimeout(function(){canPlayerChoose = true;}, 14000)
+	}
 }
 
 
@@ -56,28 +49,50 @@ function aiChoice() {
 
 }
 
-pokeBeat[1] == [3, 4]
-
 function findWinner() {
   if (pokeBeat[playerChoiceIndex][0] == aiChoiceIndex || pokeBeat[playerChoiceIndex][1] == aiChoiceIndex) {
-    alert('Player wins!');
+    textBox(`${pokeNames[aiChoiceIndex]} faints.`, 3000);
   } else if (pokeBeat[aiChoiceIndex][0] == playerChoiceIndex || pokeBeat[aiChoiceIndex][1] == playerChoiceIndex) {
-    alert('Player lost.');
+		textBox(`${pokeNames[playerChoiceIndex]} faints.`, 3000);
   } else {
-    alert('Draw.');
+    textBox(`Both pokemon faint.`, 3000)
   }
   pokeBeat[playerChoiceIndex]
 }
 
 function showPlayerChoice() {
   // show textbox "You chose X pokemon"
-  playerChoiceDivContent = pokeNames[playerChoiceIndex] + pokeImages[playerChoiceIndex];
+  playerChoiceDivContent = pokeImages[playerChoiceIndex];
   updateView();
 }
 
 
 function showAiChoice() {
   // show textbox "Your opponent chose X pokemon"
-  aiChoiceDivContent = pokeNames[aiChoiceIndex] + pokeImages[aiChoiceIndex];
+  aiChoiceDivContent = pokeImages[aiChoiceIndex];
   updateView();
+}
+
+function textBox(text = "", delay = 1000) {
+    textBoxVar = "textBoxDiv";
+    //textBoxContent = `<div class="${textBoxVar}">${text}</div>`;
+		textScroll(text);
+		updateView();
+		//sleep(2000);
+    setTimeout(function(){
+			textBoxVar = "hidden";
+			textBoxContent = `<div class="${textBoxVar}"></div>`;
+			updateView();
+		}, delay);
+    
+}
+
+function textScroll(text, curText = ''){
+	curText += text[0];
+
+	if(!text) return;
+
+	textBoxContent = `<div class="${textBoxVar}">${curText}</div>`;
+	updateView();
+	setTimeout(textScroll, 50, text.slice(1), curText);
 }
